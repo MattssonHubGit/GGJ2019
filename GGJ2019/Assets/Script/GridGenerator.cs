9d183d762e7a428aa776509dfade77ad48d8cda1
MattssonHubGit <d16erima@student.his.se>
2019-01-25 18:27:07 +0100

    blergh

---
+++ GGJ2019/Assets/Script/GridGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+
+    [SerializeField] private GameObject tilePrefab;
+
+    [Header("Level Size")]
+    [Range(1, 20)] [SerializeField] private int xSize = 1;
+    [Range(1, 20)] [SerializeField] private int ySize = 1;
+
+}
