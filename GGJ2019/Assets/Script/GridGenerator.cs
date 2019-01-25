bc764951041d79040ddbde50c51537e70d8fe942
MattssonHubGit <d16erima@student.his.se>
2019-01-25 21:07:42 +0100

    grid

--- GGJ2019/Assets/Script/GridGenerator.cs
+++ GGJ2019/Assets/Script/GridGenerator.cs
@@ -4,10 +4,78 @@ using UnityEngine;
 
 public class GridGenerator : MonoBehaviour {
 
-    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private GridTile tilePrefab;
+    private GridTile[,] level;
 
     [Header("Level Size")]
     [Range(1, 20)] [SerializeField] private int xSize = 1;
     [Range(1, 20)] [SerializeField] private int ySize = 1;
+    [Range(0, 5)] [SerializeField] private float xOffset = 2f;
+    [Range(0, 5)] [SerializeField] private float yOffset = 2f;
 
+    private void Start()
+    {
+        
+    }
+
+   private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            GenerateLevel();
+        }
+    }
+
+    [ContextMenu("New Level")]
+    private void GenerateLevel()
+    {
+
+        //Generate new Level
+        level = new GridTile[xSize, ySize];
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                GridTile _tile = Instantiate(tilePrefab, new Vector3(0 + x * xOffset, 0, 0 + y * yOffset), Quaternion.identity, this.transform);
+                _tile.gameObject.name = "Tile: " + x + ", " + y;
+                level[x, y] = _tile;
+            }
+        }
+
+        //Set up neighbours
+        for (int x = 0; x < level.GetLength(0); x++)
+        {
+            for (int y = 0; y < level.GetLength(1); y++)
+            {
+                //north
+                if (y < level.GetLength(1) - 1)
+                {
+                    level[x, y].neighbourNorth = level[x, y + 1];
+                    level[x, y].neighbours.Add(level[x, y + 1]);
+                }
+
+                //south
+                if (y > 0)
+                {
+                    level[x, y].neighbourSouth = level[x, y - 1];
+                    level[x, y].neighbours.Add(level[x, y - 1]);
+                }
+
+                //east
+                if (x < level.GetLength(0) - 1)
+                {
+                    level[x, y].neighbourEast = level[x + 1, y];
+                    level[x, y].neighbours.Add(level[x + 1, y]);
+                }
+
+                //west
+                if (x > 0)
+                {
+                    level[x, y].neighbourWest = level[x -1, y];
+                    level[x, y].neighbours.Add(level[x - 1, y]);
+                }
+
+            }
+        }
+    }
 }
---
+++ GGJ2019/Assets/Script/GridTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour {
+    
+    [Header("Required Prefab Components")]
+    public Transform pointToStand;
+
+    [Header("Neighbours")]
+    public GridTile neighbourNorth;
+    public GridTile neighbourSouth;
+    public GridTile neighbourEast;
+    public GridTile neighbourWest;
+
+    public List<GridTile> neighbours = new List<GridTile>();
+
+
+    [Header("Accessibility")]
+    public bool isBlocking = false;
+
+}
