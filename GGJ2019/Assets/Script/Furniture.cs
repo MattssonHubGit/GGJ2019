1538cc18f4faaf451d7e12a50f61887c15bb7530
MattssonHubGit <d16erima@student.his.se>
2019-01-25 22:04:00 +0100

    simple level editing

---
+++ GGJ2019/Assets/Script/Furniture.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furniture : Obsticle {
+
+    [SerializeField] private bool eternal = false;
+
+    public override void OnEnter()
+    {
+        Debug.Log("You shouldn't be able to enter furniture!");
+    }
+
+    public override void OnAttemptEnter()
+    {
+        Debug.Log("You can't enter furniture!");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("And stay out of the furniture!");
+    }
+
+    public override void OnAttemptExit()
+    {
+        Debug.Log("Yeah, get out of the furniture!");
+    }
+}
--- GGJ2019/Assets/Script/GridTile.cs
+++ GGJ2019/Assets/Script/GridTile.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GridTile : MonoBehaviour {
     
@@ -15,8 +16,56 @@ public class GridTile : MonoBehaviour {
 
     public List<GridTile> neighbours = new List<GridTile>();
 
-
     [Header("Accessibility")]
     public bool isBlocking = false;
 
+    #region Level editing
+    [Header("Level")]
+    [SerializeField] private Obsticle myObsticle = null;
+    public UnityEvent SetUpObsticle;
+
+    [ContextMenu("Set up Obsticles")]
+    private void SetUpObsticles()
+    {
+        SetUpObsticle.Invoke();
+    }
+
+    public void AddBlocker(Obsticle obsticlePrefab)
+    {
+        Obsticle _obsticle = Instantiate(obsticlePrefab, pointToStand.position, Quaternion.identity, this.transform);
+        isBlocking = _obsticle.blocks;
+        myObsticle = _obsticle;
+    }
+    #endregion
+
+    public void OnEnter()
+    {
+        if (myObsticle != null)
+        {
+            myObsticle.OnEnter();
+        }
+    }
+
+    public void OnAttemptEnter()
+    {
+        if (myObsticle != null)
+        {
+            myObsticle.OnAttemptEnter();
+        }
+    }
+
+    public void OnExit()
+    {
+        if (myObsticle != null)
+        {
+            myObsticle.OnExit();
+        }
+    }
+    public void OnAttemptExit()
+    {
+        if (myObsticle != null)
+        {
+            myObsticle.OnAttemptExit();
+        }
+    }
 }
