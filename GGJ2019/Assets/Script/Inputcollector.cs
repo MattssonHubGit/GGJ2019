1baf0bd6eb9dc78bb512059c175178cf81e9cd60
addamoom <gst12124@hs.local>
2019-01-25 21:09:08 +0100

    a little bit of movment, input collector

--- GGJ2019/Assets/Script/Inputcollector.cs
+++ GGJ2019/Assets/Script/Inputcollector.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inputcollector : MonoBehaviour {
+public class InputCollector : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    List<string> keylist;
+    public static InputCollector Instance;
+
+    // Use this for initialization
+    void Start() {
+        keylist = new List<string>();
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+            Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown("up"))
+            keylist.Add("up");
+        if (Input.GetKeyDown("down"))
+            keylist.Add("down");
+        if (Input.GetKeyDown("left"))
+            keylist.Add("left");
+        if (Input.GetKeyDown("right"))
+            keylist.Add("right");
+        if (Input.GetKeyDown("space"))
+            keylist.Add("space");
+
+    }
+
+    public void Emptykeylist() {
+        keylist.Clear();
+    }
+
+    public List<string> Getcurrentkeylist(){
+        return keylist;
+    }
 }
---
+++ GGJ2019/Assets/Script/Movment.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour {
+
+    bool isready;
+    List<string> keylist;
+    int i;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (Input.GetKeyDown("enter")){
+
+            keylist = InputCollector.Instance.Getcurrentkeylist();
+
+            for (i = 0; i < keylist.Count; i++)
+            {
+                switch (keylist[i])
+                {
+                    case "up"
+            
+                  
+                }
+            }
+
+
+
+
+
+
+        }
+	}
+}
