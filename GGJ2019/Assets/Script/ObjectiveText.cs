a5fb74df8430b49c5454a4c52be0a3c5d1bd894e
MattssonHubGit <d16erima@student.his.se>
2019-01-26 22:35:39 +0100

    UI

---
+++ GGJ2019/Assets/Script/ObjectiveText.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveText : MonoBehaviour {
+
+    [SerializeField] public Text textToEdit;
+}
