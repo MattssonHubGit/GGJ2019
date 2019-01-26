409448bcd1c57be74af5cdcf44c8a840b7239c90
addamoom <gst12124@hs.local>
2019-01-26 12:25:18 +0100

    Movment kanske funkar eventuellt

--- GGJ2019/Assets/Script/Movment.cs
+++ GGJ2019/Assets/Script/Movment.cs
@@ -9,58 +9,11 @@ public class Movment : MonoBehaviour {
     int i;
     GridTile currentTile;
     GridTile targetTile;
-    public Player player;
+    
 
 	void Start () {
 		
 	}
 	
-	void Update () {
-
-        if (Input.GetKeyDown("enter")){
-
-            keylist = InputCollector.Instance.Getcurrentkeylist();
-            
-            for (i = 0; i < keylist.Count; i++)
-            {
-                currentTile= player.currentTile;
-
-
-                switch (keylist[i])
-                {
-                    case "up":
-                        targetTile = currentTile.neighbourNorth;
-                        if (!targetTile.isBlocking)
-                        {
-                            Player.Moveto(targetTile);
-                        }
-
-                        break;
-                    case "down":
-
-                        break;
-                    case "left":
-
-                        break;
-                    case "right":
-
-                        break;
-                    case "space":
-                        //WaitForSecondsRealtime(1) eller nått
-                        break;
-                    default:
-                        print("skumma saker hände");
-                        break;
-
-
-                }
-            }
-
-
-
-
-
-
-        }
-	}
+	
 }
--- GGJ2019/Assets/Script/Player.cs
+++ GGJ2019/Assets/Script/Player.cs
@@ -6,10 +6,130 @@ public class Player : MonoBehaviour {
 
     public GridTile currentTile = null;
     Transform targetPos;
+    public GridTile targetTile;
+    List<string> keylist;
+    int i;
+
+    public enum PlayerState { WAITING, EXECUTING, OTHER};
+    private PlayerState currentState = PlayerState.OTHER;
 
     public void Moveto(GridTile targetTile)
     {
-        transform.position = Vector3.MoveTowards(transform.position, targetTile.pointToStand.position, Time.deltaTime);
+        
     
+    }
+    void Update()
+    {
+
+        switch (currentState)
+        {
+            case PlayerState.WAITING:
+
+                break;
+            case PlayerState.EXECUTING:
+                CommandController();
+                break;
+            case PlayerState.OTHER:
+
+                break;
+            default:
+                break;
+        }
+
+        /*if (Input.GetKeyDown("enter"))
+        {
+
+            keylist = InputCollector.Instance.Getcurrentkeylist();
+
+            for (i = 0; i < keylist.Count; i++)
+            {
+
+                //här måste jag ta reda på vilken tile som är currentTile
+
+                switch (keylist[i])
+                {
+                    case "up":
+                        targetTile = currentTile.neighbourNorth;
+                        if (!targetTile.isBlocking)
+                        {
+                            StartCoroutine(Move(targetTile));  
+                        }
+
+                        break;
+                    case "down":
+
+                        break;
+                    case "left":
+
+                        break;
+                    case "right":
+
+                        break;
+                    case "space":
+
+                        break;
+                    default:
+                        print("skumma saker hände");
+                        break;
+
+
+                }
+            }
+        }*/
+    }
+    IEnumerator Move(GridTile targetTile)
+    {
+        while (Vector3.Distance(transform.position, targetTile.transform.position) > 0.01)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetTile.pointToStand.position, Time.deltaTime);
+
+            yield return null;
+        }
+
+        currentState = PlayerState.EXECUTING;
+    }
+
+    private void CommandController() {
+        if (Input.GetKeyDown("enter"))
+        {
+
+            keylist = InputCollector.Instance.Getcurrentkeylist();
+
+            for (i = 0; i < keylist.Count; i++)
+            {
+
+                //här måste jag ta reda på vilken tile som är currentTile
+
+                switch (keylist[i])
+                {
+                    case "up":
+                        targetTile = currentTile.neighbourNorth;
+                        if (!targetTile.isBlocking)
+                        {
+                            currentState = PlayerState.WAITING;
+                            StartCoroutine(Move(targetTile));
+                        }
+
+                        break;
+                    case "down":
+
+                        break;
+                    case "left":
+
+                        break;
+                    case "right":
+
+                        break;
+                    case "space":
+
+                        break;
+                    default:
+                        print("skumma saker hände");
+                        break;
+
+
+                }
+            }
+        }
     }
 }
